d403f5b9f2d43e46af68e89a918307b61c98a934
Mason Reichbauer <mreichba@mail.usf.edu>
2020-01-23 14:09:21 -0500

    on 5

--- main.js
+++ main.js
@@ -9,13 +9,20 @@ function main() {
   let arr = new Array();
 
   // Add an item to the array
-  arr.push(3);
-  arr.push(5);
-  arr.push(15);
-  arr.push(19);
-  arr.push(45);
-  arr.push(10);
-  console.log(arr);
+  // arr.push(3);//length: 1, capac: 3, ptr: 0
+  // arr.push(5);
+  // arr.push(15); //here capacity maxes out then adds one and multiplies by 4 (3*4=12) (calls resize())
+  // arr.push(19);
+  // arr.push(45);
+  // arr.push(10);//length: 6, capac: 12, ptr: 3  cap is set from above
+  // console.log(arr);
+  // arr.pop();
+  // arr.pop();
+  // arr.pop();//length: 3, capac: 12, ptr: 3 //only length changes not capacity or ptr
+  // 
+  // console.log(arr.get(0));
+  // arr.push('mason'); //memory only accepts numbers
+  console.log(arr.get(0));
 }
 
 main();
